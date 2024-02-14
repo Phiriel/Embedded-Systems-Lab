7da2545a30cfc689925a026dd9f0543ac79634df
BeeseChurger <72339360+Phiriel@users.noreply.github.com>
2024-02-13 22:35:10 -0700

    Lab-02

---
+++ Lab02/Lab1/MDK-ARM/RTE/_Lab1/RTE_Components.h
@@ -0,0 +1,15 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'Lab1' 
+ * Target:  'Lab1' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+
+#endif /* RTE_COMPONENTS_H */
